57b30de9b8f0045a0e3d81c9e24683942e9f4245
bhagyashreebhopale <bhagyashree.bhopale12@gmail.com>
2020-04-13 12:51:43 +0530

    ff

---
+++ sonarQube/com.demo/src/main/java/com/demo/Application.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+public class Application {
+ public static void main(String args[]) {System.out.println("Hello");}
+ 
+ class A{
+	 private String a="";
+	 A(){}
+	 
+	 public void dummy(int num)
+	 {
+		 num=10;
+		 if(num==0)
+		 {
+			 System.out.println("Zero");
+		 }
+	 }
+	 
+ }
+ 
+}
